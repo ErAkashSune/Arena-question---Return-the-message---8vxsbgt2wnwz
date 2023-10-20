c9ee1f924417606680ef225d6485300f925c6442
ErAkashSune <erakashbhimraosune@gmail.com>
2023-10-20 15:15:20 +0530

    cummit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
-const Header = () => {}
+const Header = () => {
+   return(
+     <div id='header-component'>
+        Newton School
+     </div>
+   );
+}
 
 
 export default Header;
